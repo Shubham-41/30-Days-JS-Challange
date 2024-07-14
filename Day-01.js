0cf4a10640b8690e34b02a3b17fa99225143defb
LIPANE SHUBHAM SUNIL <90514327+Shubham-41@users.noreply.github.com>
2024-07-14 09:54:30 +0530

    Add files via upload

---
+++ Day-01.js
@@ -0,0 +1,95 @@
+
+// Activity 01 :- Variable Declaration 
+
+// Task 01 :- Declare a variable using var, assign it a number, and log the value to the console.
+
+var num = 74 ;
+
+console.log(num);
+
+// Task 02 :- Declare a variable using let, assign it a string, and log the value to the console.
+
+let str = "Shubham" ;
+
+console.log(str);
+
+// Activity 2: Constant Declaration
+
+// Task 03 :- Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+const boolvalueT = true ;
+
+console.log(boolvalueT);
+
+const boolvalueF = false ;
+
+console.log(boolvalueF);
+
+
+// Activity 3: Data Types
+
+// Task 04 :- Create variables of different data types (number,string,boolean,object,array) and log each variable's type using the typeof operator.
+
+let varnum = 45 ;
+
+console.log(typeof varnum);
+
+let varstr = "Dhruva" ;
+
+console.log(typeof varstr);
+
+let varbool = true ;
+
+console.log(typeof varbool);
+
+let myobj = {
+    name: "Vaibhav",
+    Clg:"ICT-Jalan",
+    age:19,
+}   // Object Declaration using let 
+
+console.log(typeof myobj);
+
+/*
+
+const person = {
+    firstname: "Shubham",
+    lastname:   "Lipane"
+};  // Object Declaration using const
+
+console.log(typeof person);
+
+*/
+
+const shopping_list = ["bread","milk","yoghurt"];        // Array Declaration
+
+console.log(typeof shopping_list);
+
+// Activity 04 :- Reassigning Variables
+
+// Task 05 : Declare a variable using let, assign it an intial value, reassign a new value, and log both values to the console
+
+let new_Val = 25 ;
+
+console.log(new_Val);   // Printing Intial Value
+
+new_Val = 40 ;
+
+console.log(new_Val);   // Printing the newely assigned Value 
+
+
+// Activity 05 :- Understanding const
+
+// Task 06 : Try reassigning a variable declared with const and observe the error
+
+const constVar = 91 ;
+
+console.log(constVar);
+
+constVar = 97 ; // Error :--> TypeError: Assignment to constant variable.
+
+
+
+
+
+
